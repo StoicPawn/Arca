e8ca4245a3da9640def2e8c65284cb4c74765173
StoicPawn <151366316+StoicPawn@users.noreply.github.com>
2025-10-16 22:06:10 +0200

    Delete src directory

--- src/data/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Data module for Arca."""
-from .datasets import prepare_datasets
-from .augment import build_audio_augmentations, build_vision_augmentations
-
-__all__ = ["prepare_datasets", "build_audio_augmentations", "build_vision_augmentations"]
